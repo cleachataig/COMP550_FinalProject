f1186ca1eab079956301910a8245cc8f17b9dbd8
Rousset Thibault <thibault.rousset@student-cs.fr>
2023-12-07 15:15:21 -0500

    Cleaning Up

--- main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from gutenberg.acquire import load_etext
-from gutenberg.cleanup import strip_headers
-
-text = strip_headers(load_etext(2701)).strip()
-print(text)  # prints 'MOBY DICK; OR THE WHALE\n\nBy Herman Melville ...'
